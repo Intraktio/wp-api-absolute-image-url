42aed2a4476cfd000b40a013640b9037e30ec48f
Karri Rasinmäki <karri.rasinmaki@gmail.com>
2018-03-20 19:27:10 +0200

    Attachment image urls to absolute

--- wp-api-absolute-image-url.php
+++ wp-api-absolute-image-url.php
@@ -13,7 +13,7 @@ Domain Path:    /languages
 class WP_API_Absolute_Image_Url {
 
     function init() {
-        add_action('parse_request', array($this, 'add_filters_to_api_requests'), 3, 1);
+        add_action( 'parse_request', array($this, 'add_filters_to_api_requests'), 3, 1 );
     }
 
     function contains_string($string, $substring) {
@@ -35,10 +35,32 @@ class WP_API_Absolute_Image_Url {
         try {
             $is_rest = array_key_exists('rest_route', $query->query_vars);
             if( $is_rest === true ) {
-                add_filter('wp_calculate_image_srcset', array($this, 'append_site_url_to_srcset'), 10, 5);
+                add_filter( 'wp_calculate_image_srcset', array($this, 'append_site_url_to_srcset'), 10, 5 );
+	        add_filter( 'wp_get_attachment_image_src', array( $this, 'handle_attachment_image_urls' ), 10, 4 );
             }
         } catch (Exception $e) {}
         return $query;
+    }
+
+    /**
+     * Converts relative attachment image urls to absolute.
+     *
+     * @param array|false  $image         Either array with src, width & height, icon src, or false.
+     * @param int          $attachment_id Image attachment ID.
+     * @param string|array $size          Size of image. Image size or array of width and height values
+     *                                    (in that order). Default 'thumbnail'.
+     * @param bool         $icon          Whether the image should be treated as an icon. Default false.
+     */
+    public function handle_attachment_image_urls( $image, $attachment_id, $size, $icon ) {
+            if ( $image === false ) {
+                    return $image;
+            }
+            $url = $image[0];
+            if ( !preg_match("/^[a-zA-Z]+:\/\//", $url) ) {
+                    $url = get_site_url() . $url;
+                    $image[0] = $url;
+            }
+            return $image;
     }
 
 }
